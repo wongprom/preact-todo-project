a481ac4b0d34632c9b798f381e27fd8abe2bae9b
wongprom <>
2023-10-25 21:41:51 +0200

    Create visual struction.

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,24 @@
 import { render } from 'preact';
-import { Button } from '@/components/ui/button';
 import './style.css';
 
 export function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-600">
-        Hello world!
-      </h1>
-      <div className="flex items-center ">
-        <Button size="lg">Click me</Button>
-      </div>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className=" text-4xl font-bold underline text-red-600">Todo List</h1>
+      <section className="todos">
+        <h2 className="text-3xl">Todos</h2>
+        <div>{/* map all todos with table */}</div>
+      </section>
+      <section className="complitedTodos">
+        <h2 className="text-3xl">Complited Todos</h2>
+        <div>{/* map all complited todos with table */}</div>
+      </section>
+      <section className="addNewTodo">
+        <h2 className="text-3xl">Add New Todo</h2>
+        <div>{/* set up form with one input  */}</div>
+      </section>
     </div>
   );
 }
-
-// function Resource(props) {
-//   return (
-//     <a href={props.href} target="_blank" class="resource">
-//       <h2>{props.title}</h2>
-//       <p>{props.description}</p>
-//     </a>
-//   );
-// }
 
 render(<App />, document.getElementById('app'));
