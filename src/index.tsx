fe244ee5da93808de5184ddc139379fefad5d1d7
wongprom <>
2023-10-26 22:17:34 +0200

    Add todo in table shadcn.

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,44 @@
 import { render } from 'preact';
 import './style.css';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { Checkbox } from '@/components/ui/checkbox';
 
 export function App() {
   return (
     <div className="flex flex-col items-center justify-center">
-      <h1 className=" text-4xl font-bold underline text-red-600">Todo List</h1>
-      <section className="todos">
-        <h2 className="text-3xl">Todos</h2>
-        <div>{/* map all todos with table */}</div>
+      <h1 className="text-4xl font-bold underline text-center text-red-600">
+        Your Todo List
+      </h1>
+      <section className="todos m-3 p-3 bg-gray-300">
+        <h2 className="text-3xl text-center">Todos</h2>
+        <div>
+          <Table>
+            <TableCaption>A list of all your Todos.</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="max-w-[200px]">Your Todos</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow className="p-2">
+                <Checkbox />
+                <TableCell className="font-medium">Shopping</TableCell>
+              </TableRow>
+              <TableRow className="p-2">
+                <Checkbox />
+                <TableCell className="font-medium">Clean CoCo</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
       </section>
       <section className="complitedTodos">
         <h2 className="text-3xl">Complited Todos</h2>
